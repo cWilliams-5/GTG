80a7e0f6f69760ed1c0046c22a742f6c1c8914f4
clayton w <rasaec@gmail.com>
2022-08-23 18:26:02 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TaskItem from './component/TaskItem';
+
+function App() {
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <TaskItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/TaskItem.js
@@ -0,0 +1,12 @@
+
+function TaskItem() {
+
+    return (
+        <div>
+            <h2>Task</h2>
+            <h2>Date to be Completed</h2>
+        </div>
+    );
+
+}
+export default TaskItem;
