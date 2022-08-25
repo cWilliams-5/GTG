57eb7ad31fbf285c8074c1f487f1219199e16d9b
Clayton <rasaec@gmail.com>
2022-08-25 15:45:56 -0600

    Commit # 4 every thing works except for the to do list doent change, but the changes can be seen in the console log

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import TaskItem from './component/TaskItem';
-//import Display from './component/display';
+
 
 
 function App() {
@@ -37,7 +37,7 @@ function App() {
         break;
       }
     }
-    // var objIndex = taskTitle.findIndex((obj => obj.title == task))
+
   }
   function editDate() {
     let task = prompt('Task to edit');
@@ -53,7 +53,7 @@ function App() {
     <div>
       <h1>To-Do List</h1>
       <TaskItem title= 'Task' dueDate= {new Date()}></TaskItem>
-      <script src='C:\Users\rasae\OneDrive\Desktop\School\Bootcamps\FullStackGtG\assignments\to-do\src\component\newTask.js'></script>
+      
       <button onClick = {newTask} >Create New Task</button><button onClick ={editTask}>Edit Task</button>
       <button onClick = {editDate}>Edit Date</button>
     
@@ -62,9 +62,9 @@ function App() {
       <TaskItem title = {taskTitle[1].title} dueDate = {taskTitle[1].dueDate}></TaskItem>
 
       <TaskItem title = {taskTitle[2].title} dueDate = {taskTitle[2].dueDate}></TaskItem>
-      {/* <Display taskTitle></Display> */}
+
     </div>
   );
 }
-// title = 'Title' dueDate = 'Date'
+
 export default App;
---
+++ src/component/newTask.js
@@ -0,0 +1,13 @@
+
+
+function newTask(taskTitle) {
+    const tempTask = {}
+    let title1 = prompt('New Task');
+    let date1 = prompt('Due Date');
+    tempTask['title'] = title1
+    tempTask['date'] = date1
+    taskTitle.push(tempTask);
+    console.log(tempTask);
+    console.log(taskTitle);
+    return (taskTitle);
+  }
